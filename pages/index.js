9555d22dee277844a9481d08440f40e186b9a459
BurningCode1 <154074703+BurningCode1@users.noreply.github.com>
2024-01-04 22:47:24 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-   return <h1> ola mundo,tenham força e determinaçao para seguir em frente,atraves de tudo em seu caminho!  </h1>
+   return <h1> ola mundo,tenham força e determinaçao para seguir em frente,atraves de tudo em seu caminho!!  </h1>
 }
 
 export default Home;
