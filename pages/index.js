54397c99d2e482b9a9e85205ac1e84858ca3c443
BurningCode1 <154074703+BurningCode1@users.noreply.github.com>
2024-01-06 20:52:19 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 function Home() {
-   return <h1> ola mundo,tenham força e determinaçao para seguir em frente,atraves de tudo em seu caminho!!  </h1>
+  return (
+    <h1>
+      {" "}
+      ola mundo,tenham força e determinaçao para seguir em frente,atraves de
+      tudo em seu caminho!!{" "}
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentaçao errada");
 }
 
 export default Home;
