81ba32adc928af2f406acfdd30e3b671427f4ea8
BurningCode1 <154074703+BurningCode1@users.noreply.github.com>
2024-06-11 22:44:33 +0000

    adds `api/v1/migrations` endpoint

---
+++ infra/migrations/1716245478278_test-migration.js
@@ -0,0 +1,5 @@
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {};
+
+exports.down = (pgm) => {};
---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,38 @@
+import migrationRunner from "node-pg-migrate";
+import { join } from "node:path";
+import database from "infra/database.js";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    dryRun: true,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
+
+  if (request.method === "GET") {
+    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+    await dbClient.end();
+    return response.status(200).json(pendingMigrations);
+  }
+
+  if (request.method === "POST") {
+    const migrateMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if (migrateMigrations.length > 0) {
+      return response.status(201).json(migrateMigrations);
+    }
+    return response.status(200).json(migrateMigrations);
+  }
+
+  return response.status(405).end();
+}
