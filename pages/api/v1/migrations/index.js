bd1011d2ef13bf98b9f34dba441e00465bd5230f
BurningCode1 <154074703+BurningCode1@users.noreply.github.com>
2024-06-23 18:53:39 +0000

    resolution fix-migrations bug connection

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,69 +3,47 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewClient();
-  let db = dbClient;
-
-  const defaultMigrationOptions = {
-    dbClient: db,
-    dryRun: true,
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-    try {
-      db;
-    } catch (error) {
-      console.log(error);
-      throw error;
-    } finally {
-      await dbClient.end();
-    }
-    return response.status(200).json(pendingMigrations);
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `method "${request.method}" not allowed `,
+    });
   }
 
-  if (request.method === "POST") {
-    const migrateMigrations = await migrationRunner({
-      ...defaultMigrationOptions,
-      dryRun: false,
-    });
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
 
-    const databaseMaxConnectionsResult = await database.query(
-      "SHOW max_connections;",
-    );
-    const databaseMaxConnectionsValue =
-      databaseMaxConnectionsResult.rows[0].max_connections;
-    const databaseName = process.env.POSTGRES_DB;
-    const databaseOpenedConnectionsResult = await database.query({
-      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-      values: [databaseName],
-    });
+    const defaultMigrationOptions = {
+      dbClient: dbClient,
+      dryRun: true,
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
 
-    const databaseOpenedConnectionsValue =
-      databaseOpenedConnectionsResult.rows[0].count;
-    console.log(databaseOpenedConnectionsValue);
-    try {
-      db;
-    } catch (error) {
-      console.log(error);
-      throw error;
-    } finally {
-      await dbClient.end();
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+
+      return response.status(200).json(pendingMigrations);
     }
 
-    if (migrateMigrations.length > 0) {
-      return response.status(201).json({
-        ...migrateMigrations,
-        max: databaseMaxConnectionsValue,
-        open: databaseOpenedConnectionsValue,
+    if (request.method === "POST") {
+      const migrateMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
       });
-    }
-    return response.status(200).json(migrateMigrations);
-  }
 
-  return response.status(405).end();
+      if (migrateMigrations.length > 0) {
+        return response.status(201).json(migrateMigrations);
+      }
+      return response.status(200).json(migrateMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end();
+  }
 }
